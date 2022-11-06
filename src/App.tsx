a917aab0433bf691d25fa21d5a0e3b9c96dc8e46
Adam Jędrzejec <adamjedrzejec@gmail.com>
2022-11-06 12:40:10 +0100

    The second lecture progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import CreatePostForm from './components/CreatePostForm'
 
 import Posts from './components/Posts'
@@ -6,57 +6,80 @@ import Posts from './components/Posts'
 import PostType from './types/Post'
 
 function App() {
-  const [posts, setPosts] = useState<PostType[]>([
-    {
-      author: 'Thomas',
-      content: "Winter is comming, can't wait to ski!",
-      createdAt: '2022-10-23T01:19:35.631Z',
-      id: '7248e565-d819-47d5-b779-b4665e1abf21',
-      highlighted: false,
-    },
-    {
-      author: 'Ellen',
-      content: 'Hello there!',
-      createdAt: '2022-10-23T01:19:47.673Z',
-      id: '77d55611-66df-4df0-b36b-edf1e851432e',
-      highlighted: true,
-    },
-    {
-      author: 'Chris',
-      content: 'Life is beautiful.',
-      createdAt: '2022-10-23T01:20:15.785Z',
-      id: 'dc000d86-30e1-418e-94ae-e448e23f5765',
-      highlighted: false,
-    },
-    {
-      author: 'Adam',
-      content: 'Nowy post',
-      createdAt: '2022-10-23T09:10:09.902Z',
-      id: '8dcc6074-233a-4575-b220-19492164b1fd',
-      highlighted: false,
-    },
-  ])
+  const [posts, setPosts] = useState<PostType[]>([])
+  const [loading, setLoading] = useState(false)
 
-  const addNewPost = useCallback(
-    (post: PostType) => {
-      setPosts([post, ...posts])
+  const fetchPosts = useCallback(async () => {
+    setLoading(true)
+    try {
+      const response = await fetch('https://api.m4ynor.com/posts')
+      const data: PostType[] = await response.json()
+      data.sort(
+        (a, b) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      )
+      setPosts(data)
+    } catch (e) {
+      console.error('Failed to fetch posts.', e)
+    }
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchPosts()
+  }, [fetchPosts])
+
+  const handleCreatePost = useCallback(
+    async (post: PostType) => {
+      const response = await fetch('https://api.m4ynor.com/posts', {
+        body: JSON.stringify(post),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'post',
+      })
+      // const data = await response.json()
+      fetchPosts()
+    },
+    [fetchPosts]
+  )
+
+  const toggleHighlighted = useCallback(
+    async (id: string) => {
+      const post = posts.find((p) => p.id === id)
+
+      if (!post) {
+        return
+      }
+
+      const response = await fetch(`https://api.m4ynor.com/posts/${id}`, {
+        body: JSON.stringify({ ...post, highlighted: !post.highlighted }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'put',
+      })
+
+      if (response.status !== 200) {
+        return
+      }
+
+      setPosts(
+        posts.map((p) => (p.id === id ? { ...p, highlighted: !p.highlighted } : p))
+      )
     },
     [posts]
   )
 
-  const toggleHighlighted = useCallback((id: string) => {
-    setPosts((posts) =>
-      posts.map((post) =>
-        post.id === id ? { ...post, highlighted: !post.highlighted } : post
-      )
-    )
-  }, [])
-
   return (
     <div className="container">
-      Some text
-      <CreatePostForm onSubmit={addNewPost} />
-      <Posts posts={posts} toggleHighlighted={toggleHighlighted} />
+      Aplikacja
+      <CreatePostForm onSubmit={handleCreatePost} />
+      {loading ? (
+        <div>Ładowanie...</div>
+      ) : (
+        <Posts posts={posts} toggleHighlighted={toggleHighlighted} />
+      )}
     </div>
   )
 }
